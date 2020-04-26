73ce5fd8f15f30fe7631f7df522b15dad7f2a733
Amundeep Singh <amundeep@gmail.com>
2020-04-25 22:57:25 -0700

    Fixed dependencies and added schema validation

--- music_library_analysis/mla.py
+++ music_library_analysis/mla.py
@@ -1,45 +1,42 @@
 """Music Library Analysis
     
-    Usage:
-        mla.py -a
-        mla.py -h | --help
-        mla.py <required> [-f | -g | -o ]
-        mla.py <repeating>...
-    Options:
-        -h,--help       : show this help message
-        -a              : analyze music in current directory
-        repeating       : example of repeating arguments
-        -f,--flag       : example flag #1
-        -g,--greatflag  : example of flag #2
-        -o,--otherflag  : example of flag #3
+Usage:
+    mla.py -h
+    mla.py [-i] [FILE]
+
+Arguments:
+    FILE        the MP3 file to analyze
+
+Options:
+    -h,--help   : show this help message
+    -i,--info   : analyze metadata of the specified song
+
 """
+import os
+import eyed3
 
 from docopt import docopt
+from schema import Schema, And, Or, Use, SchemaError
 
 def main():
     docopt_args = docopt(__doc__)
     """ main-entry point for program, expects dict with arguments from docopt() """
-    
-    # Notice, no checking for -h, or --help is written here.
-    # docopt will automagically check for it and use your usage string.
-    
-    # User passed the required argument
-    if docopt_args["<required>"]:
-        print("You have used the required argument: " + docopt_args["<required>"])
-        
-        # Get flags used
-        if docopt_args["--flag"]:
-            print("   with --flag\n")
-        elif docopt_args["--greatflag"]:
-            print("   with --greatflag\n")
-        elif docopt_args["--otherflag"]:
-            print("   with --otherflag\n")
-        else:
-            print("   with no flags.\n")
-    # User passed 1 or more repeating arguments
-    elif docopt_args["<repeating>"]:
-        print("You have used the repeating args:")
-        print('   ' + '\n   '.join(docopt_args["<repeating>"]) + '\n')
+
+    # print(docopt_args)
+
+    schema = Schema({
+        '--help': False,
+        '--info': True,
+        'FILE': Use(open, error='FILE should be readable')})
+
+    # Validate the docopt schema
+    try:
+        docopt_args = schema.validate(docopt_args)
+    except SchemaError as e:
+        exit(e)
+
+    if (docopt_args.get('--info') == True):
+        print("You did --info")
 
 if __name__ == "__main__":
     main()
